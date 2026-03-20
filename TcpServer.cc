1a65c4b14e82f4fa949847717e9085c5537f47c6
THEONLYHY <705419001@qq.com>
2026-03-20 04:53:16 -0400

    TcpServer

--- TcpServer.cc
+++ TcpServer.cc
@@ -1,1 +1,59 @@
 #include "TcpServer.h"
+#include "Logger.h"
+
+#include <functional>
+
+EventLoop *CheckLoopNotNull(EventLoop *loop)
+{
+    if (loop == nullptr)
+    {
+        LOG_FATAL("%s:%s:%d mainLoop is null! \n", __FILE__, __FUNCTION__, __LINE__);
+    }
+    return loop;
+}
+
+TcpServer::TcpServer(EventLoop *loop,
+                     const InetAddress &listenAddr,
+                     const std::string &nameArg,
+                     Option option)
+    : loop_(CheckLoopNotNull(loop))
+    , ipPort_(listenAddr.toIpPort())
+    , name_(nameArg)
+    , acceptor_(new Acceptor(loop, listenAddr, option = kReusePort))
+    , threadPool_(new EventLoopThreadPool(loop, name_))
+    , connectionCallback_()
+    , messageCallback_()
+    , nextConnId_(1)
+{
+    // 两个占位符_1 : ip, _2 : peerAddr
+    acceptor_->setNewConnectionCallback(std::bind(&TcpServer::newConnection,
+                                        this,
+                                        std::placeholders::_1,
+                                        std::placeholders::_2));
+}
+
+TcpServer::~TcpServer()
+{
+    
+}
+
+// 设置底层subloop的数量
+void TcpServer::setThreadNum(int numThreads)
+{
+    threadPool_->setThreadNum(numThreads);
+}
+
+// 开启服务器监听
+void TcpServer::start()
+{
+    if (started_++ == 0)
+    {
+        threadPool_->start(threaInitCallback_); 
+        loop_->runInLoop(std::bind(&Acceptor::listen, acceptor_.get()));
+    }
+}
+
+void TcpServer::newConnection(int sockfd, const InetAddress &peerAddr)
+{
+
+}
