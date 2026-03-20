fa884d8890d37520bcf33be82adfce5a9b874d86
THEONLYHY <705419001@qq.com>
2026-03-20 04:52:45 -0400

    TcpServer

---
+++ Callbacks.h
@@ -0,0 +1,20 @@
+#pragma once
+
+
+#include <memory>
+#include <functional>
+
+class Buffer;
+class TcpConnection;
+class TimeStamp;
+
+// 一个指向TcpConnection的智能指针
+using TcpConnectionPtr = std::shared_ptr<TcpConnection>;
+
+using ConnectionCallback = std::function<void (const TcpConnectionPtr&)>;
+using CloseCallback = std::function<void (const TcpConnectionPtr&)>;
+using WriteCompleteCallback = std::function<void (const TcpConnectionPtr&)>;
+
+using MessageCallback = std::function<void (const TcpConnectionPtr&,
+                                            Buffer*,
+                                            TimeStamp)>;
